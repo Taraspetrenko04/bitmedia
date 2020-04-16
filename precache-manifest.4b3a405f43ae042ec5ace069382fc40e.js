c41d114ab09a8b50a17c10a916a15d7ad1738d80
Taraspetrenko04 <Taraspetrenko04@gmail.com>
2020-04-16 22:12:28 +0300

    Updates

---
+++ precache-manifest.4b3a405f43ae042ec5ace069382fc40e.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5aae60af9d59166ffb2604447b209f2a",
+    "url": "/bitmedia/index.html"
+  },
+  {
+    "revision": "a3a3c6ef7e0411444b1f",
+    "url": "/bitmedia/static/css/2.99fb4a7b.chunk.css"
+  },
+  {
+    "revision": "790d4251516a2e34bddc",
+    "url": "/bitmedia/static/css/main.8039ebf7.chunk.css"
+  },
+  {
+    "revision": "a3a3c6ef7e0411444b1f",
+    "url": "/bitmedia/static/js/2.c1aba1f4.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/bitmedia/static/js/2.c1aba1f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "790d4251516a2e34bddc",
+    "url": "/bitmedia/static/js/main.b7496199.chunk.js"
+  },
+  {
+    "revision": "c5629a2cf1beeae3573b",
+    "url": "/bitmedia/static/js/runtime-main.9967ef41.js"
+  },
+  {
+    "revision": "be0890ff5f8bbc0835cbe8839d2412ad",
+    "url": "/bitmedia/static/media/clean.be0890ff.png"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/bitmedia/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/bitmedia/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/bitmedia/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/bitmedia/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/bitmedia/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "b2e5e023ac897ab128bb1066eda31e5c",
+    "url": "/bitmedia/static/media/header_bg.b2e5e023.png"
+  },
+  {
+    "revision": "7ebcd6e858b3cd314836a262805fcb35",
+    "url": "/bitmedia/static/media/mobile.7ebcd6e8.png"
+  },
+  {
+    "revision": "61260744a992dd8b14e002b457933140",
+    "url": "/bitmedia/static/media/secure.61260744.png"
+  }
+]);
